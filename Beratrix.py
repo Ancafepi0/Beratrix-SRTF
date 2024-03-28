77e35172baad4386091a2d8a1306b463b8f47431
camilofernandezuno@gmail.com <camilofernandezuno@gmail.com>
2024-03-28 18:28:41 -0500

    TERMINACION DEL MENU Y SUS IMPLEMENTACION SIN VERIFICAR

--- Beratrix.py
+++ Beratrix.py
@@ -1,4 +1,4 @@
-#-----------FUNCIONES PRINCIPALES-----------
+#---------------------------------------------FUNCIONES PRINCIPALES DEL MENU---------------------------------------------
 #FUNCION UTILIZADA PARA OBTENER Y VERIFICAR EL NUMERO DE PROCESOS A EJECUTAR
 def obtener_numero_procesos ():
     #AQUI SE PIDE EL NUMERO DE PROCESOS A EJECUTAR SIN VERIFICAR
@@ -42,9 +42,30 @@ def creacion_nombre():
     return proceso
 
 #FUNION UTILIZADA PARA CREAR LOS PROCESOS SI EL USUARIO NO QUIERE AGREGARLE EL NOMBRE
-def creacion_sin_nombre(int):
+def creacion_sin_nombre(numero):
     proceso=[None,0,0,0,0]
-#FUNCION UTILIZADA PARA OBTENER EL NUMERO DE RAFAGAS DEL PROCESO
+    #AQUI SE DECLARA Y SE Asigna un nombre
+    nombre = ("Proceso " + numero)
+    #AQUI SE DECLARA Y SE PIDE TIEMPO DE LLEGADA
+    tiempo_llegada_respuesta = input ("Por favor digite el tiempo de llegada del proceso:  ")
+    #METODO VERIFICAR
+    while (True):
+        try:
+            tiempo_llegada = int(tiempo_llegada_respuesta)
+            while (int(tiempo_llegada) < 0):
+                tiempo_llegada = input ("RESPUESTA INVALIDA"+"\n"+"Por favor digite nuevamente el tiempo de llegada del proceso"+ "\n")
+            break
+        except ValueError:
+            tiempo_llegada_respuesta =  input ("RESPUESTA NO VALIDA"+"\n"+"Por favor digite  nuevamente el tiempo de llegada del proceso"+ "\n")
+    #AQUI SE PIDE EL NUMERO DE RAFAGA
+    numero_rafaga = obtener_rafaga()
+    #AQUI SE AGREGAN LOS VALORES A LA LISTA LLAMADA PROCESO 1. POSICION EL NOMBRE 2. POSICIN TIEMPO LLEGADA 3. POSICION NUMERO DE RAFAGA 
+    #4. TIEMPO DE ESPERA QUE AUN NO SE AGREGA Y 5. POSICION TIEMPO_TERMINACION QUE AUN NO SE AGREGA
+    proceso [0]= nombre
+    proceso [1]= tiempo_llegada
+    proceso [2]= numero_rafaga
+    #AQUI SE HACE EL RETURN DEL PROCESO TRAS HABERLO LLENADO
+    return proceso#FUNCION UTILIZADA PARA OBTENER EL NUMERO DE RAFAGAS DEL PROCESO
 def obtener_rafaga():
     #AQUI SE PIDE EL  NUMERO DE RAGAS UTILIZADAS EN EL PROCESO SIN VERIFICAR
     respuesta = input ("Digite el numero de rafagas que utiliza el proceso: ")
@@ -59,4 +80,32 @@ def obtener_rafaga():
             respuesta =  input ("RESPUESTA NO VALIDA"+"\n"+"Por favor digite  nuevamente el numero de rafagas a utilizar en el proceso"+"\n"+"MINIMO 1, MAXIMO 12"+ "\n")
     #AQUI SE RETORNA EL NUMERO DE RAFAGAS YA VERIFICADAS
     return numero_rafaga
-
+#---------------------------------------------IMPLEMENTACION DEL MENU---------------------------------------------
+def menu ():
+    lista_procesos:[]
+    #SALUDO DE PROYECTO BERATRIX
+    print (" BIENVENIDO A BERATRIX "+"\n"+ " Aplicacion para la simulacion \n de sistemas de planificacion \n STRF ")
+    #DECLARACION DE NUMERO DE REPETICIONES
+    numero_repeticiones = obtener_numero_procesos()
+    #SOLICITUD PARA SABER QUE METODO IMPLEMENTA RPARA CREAR PROCESO (CON O SIN NOMBRE)
+    respuesta = print (" ¿Desea crear los procesos con un nombre especifico o desa que se generen automaticamente? " +"\n"
+                + "DIGITE 1 PARA ASIGNAR NOMBRES"
+                + "DIIGITE 2 PARA ASIGNAR GENERAR DE FORMA AUTOMATICA")
+    #METODO PARA VERIFICAR LA RESPUESTA DL USUARIO
+    while (True):
+        try:
+            respuesta_usuario = int(respuesta)
+            while (int(respuesta_usuario) < 1 or int(respuesta_usuario) > 2):
+                respuesta_usuario = input ("RESPUESTA INVALIDA"+"\n"+"Por favor digite nuevamente el numero de rafagas a utilizar"+"\n"+"MINIMO 1, MAXIMO 12"+ "\n")
+            break
+        except ValueError:
+            respuesta =  input ("RESPUESTA NO VALIDA"+"\n"+"Por favor digite  nuevamente el numero de rafagas a utilizar en el proceso"+"\n"+"MINIMO 1, MAXIMO 12"+ "\n")
+    #CODICIONAL PARA IMPLEMENTAR EL METODO SEGUN LA RESPUESTA DEL USUARIO
+    if (respuesta_usuario==1):
+        for i in range(0,numero_repeticiones,1):
+            obj_proceso = creacion_nombre ()
+            lista_procesos.append(obj_proceso)
+    else:
+        for i in range(0,numero_repeticiones,1):
+            obj_proceso = creacion_nombre (i)
+            lista_procesos.append(obj_proceso)
